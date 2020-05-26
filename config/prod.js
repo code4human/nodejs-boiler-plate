49dbf4cba7f31546b0cfe8b8933db9949a78ef88
code4human <tena.kim10@gmail.com>
2020-05-26 21:24:31 +0900

    mongoDB 연결 정보 보호

---
+++ config/prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI
+}
+
+//MONGO_URI는 HEROKU에서 관리해주는 value
--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express()
 const port = 5000
 
 const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
 const { User } = require("./models/User");
 
 // application/x-www-form-urlencoded 데이터 분석해서 가져온다
@@ -11,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://TenaKim:mongo2020@boilerplate-3mfre.azure.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
